5a8a981d8e27d205a2081c1038b6051c0ba6ace5
dazacher <dazacher2000@yahoo.com>
2021-07-22 06:03:45 -0700

    Scope and Nested blocks.

--- src/subprograms.adb
+++ src/subprograms.adb
@@ -3,11 +3,26 @@ with Ada.Text_IO;
 procedure Subprograms is
    package TIO renames Ada.Text_IO;
    Duplicated : Integer := 10;
+
+   procedure New_Proc is
+      Duplicated : Integer := 200;
+   begin
+      TIO.Put_Line (Integer'Image (Duplicated));
+   end New_Proc;
+
 begin
+   New_Proc;
    TIO.Put_Line (Integer'Image (Duplicated));
-   declare
+   Outer_Block : declare
       Duplicated : FLoat := 20.0;
    begin
       TIO.Put_Line(Float'Image(Duplicated));
-     end;
+      TIO.Put_Line (Integer'Image (Subprograms.Duplicated));
+      declare
+         Duplicated : Integer := 100;
+      begin
+         TIO.Put_Line (Integer'Image (Duplicated));
+         TIO.Put_Line(Float'Image(Outer_Block.Duplicated));
+      end;
+   end Outer_Block;
 end Subprograms;
