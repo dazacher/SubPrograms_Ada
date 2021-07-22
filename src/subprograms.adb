34a6fdf3397284c0f592f3c933527c6c2e814ac2
dazacher <dazacher2000@yahoo.com>
2021-07-22 05:36:08 -0700

    Learning scope of variables.

--- src/subprograms.adb
+++ src/subprograms.adb
@@ -1,6 +1,13 @@
-procedure Subprograms is
+with Ada.Text_IO;
 
+procedure Subprograms is
+   package TIO renames Ada.Text_IO;
+   Duplicated : Integer := 10;
 begin
-   --  Insert code here.
-   null;
+   TIO.Put_Line (Integer'Image (Duplicated));
+   declare
+      Duplicated : FLoat := 20.0;
+   begin
+      TIO.Put_Line(Float'Image(Duplicated));
+     end;
 end Subprograms;
